0b5838d342bf1316a313e7ec755ac0e31e849a42
Sam <68431069+SButterscotch@users.noreply.github.com>
2024-09-12 20:48:34 -0700

    task3-first commit

---
+++ task3/portfolio_flask/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
+@app.route('/cookie')
+def cookie_policy():
+    return render_template('cookie.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
